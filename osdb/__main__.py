c7ae11deeb7a472128e6e5b6c62a296d79f49be6
Leonid Kuzin (aka DgINC) <dg.inc.lcf@gmail.com>
2022-02-01 15:06:26 +0300

    WIP

--- osdb/__main__.py
+++ osdb/__main__.py
@@ -1,29 +1,40 @@
+import locale
 import os
+import gettext
+from os.path import abspath, dirname, realpath, join
 
 from nextcord import Intents
 from nextcord.ext import commands
 
 TESTING_GUILD_ID = 440725941212413952
+APP = 'base'
+WHERE_AM_I = abspath(dirname(realpath(__file__)))
+LOCALE_DIR = join(WHERE_AM_I, 'language')
+
+gettext.bindtextdomain(APP, LOCALE_DIR)
+gettext.textdomain(APP)
+ru = gettext.translation(APP, localedir=LOCALE_DIR, languages=["ru_RU"], fallback=True)
+ru.install()
 
 client = commands.Bot(command_prefix="!", intents=Intents.all())
 
 
 @client.event
 async def on_ready():
-    print("OSDB start and ready to takeover of the world!")
+    print(_("OSDB start and ready to takeover of the world!"))
 
 if os.name != "nt":
     import uvloop
     uvloop.install()
 
-client.load_extension("plugins.utilities")
-client.load_extension("plugins.roles")
+client.load_extension("osdb.plugins.utilities")
+client.load_extension("osdb.plugins.roles")
 
 # for guild_id in TESTING_GUILD_ID:
 #    guild = client.get_guild(guild_id)
 #    await guild.rollout_application_commands()
 
-client.run("NjI1NjkwNTExOTE4NzU5OTQ2.XYjNsg.cbFWuseJQCJS71wkVKwKG4fbLX0")
+client.run("NjI1NjkwNTExOTE4NzU5OTQ2.XYjNsg.6kZKorGuf2ZGKr9p67GcgCDUm-k")
 
 guild = client.get_guild(TESTING_GUILD_ID)
 guild.rollout_application_commands()
--- osdb/plugins/utilities.py
+++ osdb/plugins/utilities.py
@@ -1,4 +1,7 @@
+from datetime import time
+
 import nextcord
+import gettext
 from nextcord.ext import tasks, commands
 
 
@@ -9,6 +12,7 @@ class Utilities(commands.Cog):
         # self.batch_update.add_exception_type(asyncpg.PostgresConnectionError)
 
         self.trash_id = 936682591262769205
+        self.news_id = 763296193719435265
 
     @commands.Cog.listener()
     async def on_ready(self):
@@ -17,12 +21,19 @@ class Utilities(commands.Cog):
     def cog_unload(self):
         self.clear_trash.cancel()
 
-    @tasks.loop(minutes=5.0)
+    @tasks.loop(time=time.fromisoformat('04:00:00+03:00'))
     async def clear_trash(self):
         guild = nextcord.utils.find(lambda g: g.id == 440725941212413952, self.bot.guilds)
         channel = nextcord.utils.get(guild.text_channels, id=self.trash_id)
         await channel.purge()
-        print(f"Channel {channel} purged!")
+        print(_(f'Channel "{channel}" purged!'))
+
+    @commands.Cog.listener()
+    async def on_guild_scheduled_event_create(self, event: nextcord.ScheduledEvent):
+        guild = nextcord.utils.find(lambda g: g.id == 440725941212413952, self.bot.guilds)
+        channel = nextcord.utils.get(guild.text_channels, id=self.news_id)
+        role = guild.get_role(928713881562529842)
+        await channel.send(f"{role.mention} Create guild event {event.name}. \n With context {event.description}")
 
     @nextcord.slash_command(name="ping", description="Test bot ping/pong command.")
     async def ping(self, interaction: nextcord.Interaction, ):
